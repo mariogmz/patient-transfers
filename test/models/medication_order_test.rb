6bb994036ef5fdf8f3d1c76dd076eb858f7997df
Mario Gómez <mariogomezmtz@gmail.com>
2019-04-08 12:27:58 -0500

    Add test for medication orders to_s

--- test/models/medication_order_test.rb
+++ test/models/medication_order_test.rb
@@ -19,6 +19,7 @@
 require "test_helper"
 
 class MedicationOrderTest < ActiveSupport::TestCase
+  subject { medication_orders(:naproxen) }
   context "associations" do
     should belong_to(:frequency)
     should belong_to(:patient)
@@ -32,5 +33,10 @@ class MedicationOrderTest < ActiveSupport::TestCase
   context "attributes" do
     should define_enum_for(:unit)
     should define_enum_for(:route)
+  end
+
+  test "#to_s" do
+    expected = "Naproxen 500mg PO q6hr to relieve swelling"
+    assert_equal(expected, subject.to_s)
   end
 end
